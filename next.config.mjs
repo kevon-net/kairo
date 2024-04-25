5da80b813d6d0dd8f2ab05379fe3ab1597a6c2ab
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-04-25 14:54:14 +0300

    Update files

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	sassOptions: {
-		prependData: `@import "./styles/_mantine.scss";`,
+		prependData: `@import "./_mantine.scss";`,
 	},
 
 	images: {
@@ -14,6 +14,11 @@ const nextConfig = {
 				pathname: "/**",
 			},
 		],
+	},
+
+	webpack: config => {
+		config.externals = [...config.externals, "bcrypt"];
+		return config;
 	},
 };
 

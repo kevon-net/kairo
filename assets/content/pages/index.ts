bee5709dc4b2e0e525cb9ab702168c99912d16e9
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-05-11 11:46:30 +0300

    Update assets

--- assets/content/pages/index.ts
+++
@@ -1,5 +0,0 @@
-import home from "./home";
-
-const page = { home };
-
-export default page;
--- assets/icons/social/index.ts
+++ assets/icons/social/index.ts
@@ -1,14 +1,21 @@
-import behance from "./icons8-behance.svg";
-import credly from "./icons8-credly.svg";
-import dribble from "./icons8-dribble.svg";
-import facebook from "./icons8-facebook.svg";
-import fiverr from "./icons8-fiverr.svg";
-import instagram from "./icons8-instagram.svg";
-import linkedin from "./icons8-linkedin.svg";
-import medium from "./icons8-medium.svg";
-import twitter from "./icons8-twitter.svg";
-import whatsapp from "./icons8-whatsapp.svg";
+import icon1 from "./icons8-behance.svg";
+import icon2 from "./icons8-credly.svg";
+import icon3 from "./icons8-dribble.svg";
+import icon4 from "./icons8-facebook.svg";
+import icon5 from "./icons8-fiverr.svg";
+import icon6 from "./icons8-instagram.svg";
+import icon7 from "./icons8-linkedin.svg";
+import icon8 from "./icons8-medium.svg";
+import icon9 from "./icons8-twitter.svg";
+import icon10 from "./icons8-whatsapp.svg";
 
-const social = { behance, credly, dribble, facebook, fiverr, instagram, linkedin, medium, twitter, whatsapp };
-
-export default social;
+export const behance = icon1;
+export const credly = icon2;
+export const dribble = icon3;
+export const facebook = icon4;
+export const fiverr = icon5;
+export const instagram = icon6;
+export const linkedin = icon7;
+export const medium = icon8;
+export const twitter = icon9;
+export const whatsapp = icon10;

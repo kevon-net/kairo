76478b6870942aafd29acdbc24ca2e738a014a24
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-06-12 10:11:36 +0300

    Delete files

--- components/clerk/UserButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { UserButton as UserButtonClerk } from "@clerk/nextjs";
-
-import classes from "./UserButton.module.scss";
-
-export default function UserButton() {
-	return <UserButtonClerk appearance={{ elements: {} }} />;
-}

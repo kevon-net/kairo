810786cca4e1158c560c4c0f252e4dd74b30aeb4
Kevon Kibochi <devokrann@gmail.com>
2024-12-26 12:17:02 +0300

    todo: add colorscheme useffect

--- apps/web/src/hooks/color-scheme.ts
+++ apps/web/src/hooks/color-scheme.ts
@@ -1,8 +1,9 @@
 import { MantineColorScheme, useMantineColorScheme } from '@mantine/core';
 import { COOKIE_NAME, EXPIRY } from '@/data/constants';
-import { setCookie, getOSTheme } from '@repo/utils/helpers';
+import { setCookie, getOSTheme, getCookie } from '@repo/utils/helpers';
 import { useAppDispatch, useAppSelector } from './redux';
 import { updateColorScheme } from '@/libraries/redux/slices/color-scheme';
+import { useEffect } from 'react';
 
 export const useColorSchemeHandler = () => {
   const { setColorScheme } = useMantineColorScheme({ keepTransitions: true });
@@ -22,14 +23,22 @@ export const useColorSchemeHandler = () => {
     const scheme =
       value == 'light' ? 'light' : value == 'dark' ? 'dark' : getOSTheme();
 
+    // update mantine color scheme
+    setColorScheme(scheme as MantineColorScheme);
+
     // update scheme cookie
     setCookie(COOKIE_NAME.COLOR_SCHEME, scheme, {
       expiryInSeconds: EXPIRY.SESSION.EXTENDED.SEC,
     });
-
-    // update mantine color scheme
-    setColorScheme(scheme as MantineColorScheme);
   };
+
+  useEffect(() => {
+    const cookieValueState = getCookie(COOKIE_NAME.COLOR_SCHEME_STATE);
+    dispatch(updateColorScheme(cookieValueState));
+
+    const cookieValue = getCookie(COOKIE_NAME.COLOR_SCHEME);
+    setColorScheme((cookieValue as MantineColorScheme) || 'light');
+  });
 
   return { colorScheme, handleChange };
 };

5d60e2c0ae581fc5949f8ee2a7ae702a1f22a2f5
Kevon Kibochi <devokrann@gmail.com>
2025-01-10 07:46:27 +0300

    todo: update image assets

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,33 +1,28 @@
+const baseUrl = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images`;
+const iconUrl = `https://img.icons8.com`;
+
 export const images = {
   brand: {
     logo: {
-      light:
-        'https://spewyluxhudgxpuhjjam.supabase.co/storage/v1/object/public/logos/brand/logo-light.png',
-      dark: 'https://spewyluxhudgxpuhjjam.supabase.co/storage/v1/object/public/logos/brand/logo-dark.png',
+      light: `${baseUrl}/logos/brand/logo-light.png`,
+      dark: `${baseUrl}/logos/brand/logo-dark.png`,
     },
     icon: {
-      light:
-        'https://spewyluxhudgxpuhjjam.supabase.co/storage/v1/object/public/avatars/brand/icon/icon-light.png',
-      dark: 'https://spewyluxhudgxpuhjjam.supabase.co/storage/v1/object/public/avatars/brand/icon/icon-dark.png',
+      light: `${baseUrl}/avatars/brand/icon/icon-light.png`,
+      dark: `${baseUrl}/avatars/brand/icon/icon-dark.png`,
     },
   },
 
   icons: {
     social: {
-      facebook:
-        'https://img.icons8.com/?size=100&id=13912&format=png&color=000000',
-      instagram:
-        'https://img.icons8.com/?size=100&id=32323&format=png&color=000000',
-      twitterx:
-        'https://img.icons8.com/?size=100&id=ClbD5JTFM7FA&format=png&color=000000',
-      linkedin:
-        'https://img.icons8.com/?size=100&id=13930&format=png&color=000000',
-      whatsapp:
-        'https://img.icons8.com/?size=100&id=16713&format=png&color=000000',
+      facebook: `${iconUrl}/?size=100&id=13912&format=png&color=000000`,
+      instagram: `${iconUrl}/?size=100&id=32323&format=png&color=000000`,
+      twitterx: `${iconUrl}/?size=100&id=ClbD5JTFM7FA&format=png&color=000000`,
+      linkedin: `${iconUrl}/?size=100&id=13930&format=png&color=000000`,
+      whatsapp: `${iconUrl}/?size=100&id=16713&format=png&color=000000`,
     },
 
-    google: 'https://img.icons8.com/?size=100&id=17949&format=png&color=000000',
-    credentials:
-      'https://img.icons8.com/?size=100&id=jicLxt1sA2qa&format=png&color=000000',
+    google: `${iconUrl}/?size=100&id=17949&format=png&color=000000`,
+    credentials: `${iconUrl}/?size=100&id=jicLxt1sA2qa&format=png&color=000000`,
   },
 };

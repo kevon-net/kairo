e3d1e080c805bb062dd9c5dd027d2d950f4954ca
Kevon Kibochi <devokrann@gmail.com>
2025-01-10 07:47:44 +0300

    todo: add missing helper function

--- src/utilities/helpers/array.ts
+++ src/utilities/helpers/array.ts
@@ -55,3 +55,16 @@ export const sortArray = <T>(
 
   return [...itemsValidSorted, ...items.null];
 };
+
+export const shuffleArray = <T>(array: T[]): T[] => {
+  const shuffledArray = [...array]; // Create a copy to avoid mutating the original array
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[randomIndex]] = [
+      shuffledArray[randomIndex],
+      shuffledArray[i],
+    ];
+  }
+
+  return shuffledArray;
+};

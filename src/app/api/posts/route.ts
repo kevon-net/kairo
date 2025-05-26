9ccddb94243ff618f9e01724bd2bbdf7e6e634f6
Kevon Kibochi <dev@kevon.net>
2025-05-26 11:45:09 +0300

    chore: add route handlers

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,32 @@
+import prisma from '@/libraries/prisma';
+import { NextResponse } from 'next/server';
+
+export const dynamic = 'force-dynamic';
+export const revalidate = 3600;
+
+export async function GET() {
+  try {
+    const postRecords = await prisma.post.findMany({
+      include: {
+        _count: { select: { comments: true } },
+
+        category: true,
+        tags: true,
+        profile: true,
+      },
+
+      orderBy: { created_at: 'desc' },
+    });
+
+    return NextResponse.json(
+      { posts: postRecords },
+      { status: 200, statusText: 'Posts Retrieved' }
+    );
+  } catch (error) {
+    console.error('---> route handler error (get posts):', error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}

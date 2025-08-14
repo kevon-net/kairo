346805d033661e88bf4af1bd5f8ca251442deb39
Kevon Kibochi <dev@kevon.net>
2025-08-14 10:42:19 +0300

    chore: update home partial

--- src/components/partial/listings/home.tsx
+++ src/components/partial/listings/home.tsx
@@ -2,29 +2,25 @@
 
 import React from 'react';
 import { SECTION_SPACING } from '@/data/constants';
-import { Container, Group, Skeleton, Stack, Title } from '@mantine/core';
+import { Container, Group, Stack } from '@mantine/core';
 import { useAppSelector } from '@/hooks/redux';
-import { getTimeOfDay } from '@/utilities/helpers/time';
-import { capitalizeWords } from '@/utilities/formatters/string';
+import CardSessionLive from '@/components/common/cards/session/live';
 
 export default function Home() {
-  const session = useAppSelector((state) => state.session.value);
+  const selectedSession = useAppSelector(
+    (state) => state.sessions.selectedSession
+  );
 
   return (
-    <Container p={{ md: SECTION_SPACING }} py={SECTION_SPACING / 2}>
-      <Stack gap={SECTION_SPACING / 2}>
-        {session == null ? (
-          <Group justify="center">
-            <Skeleton height={26} width={260} />
-          </Group>
-        ) : (
-          <Title order={1} fz={'xl'} fw={500} ta={'center'}>
-            Good {getTimeOfDay()},{' '}
-            {capitalizeWords(session?.user_metadata.name)}
-          </Title>
-        )}
-
-        <div>timer</div>
+    <Container
+      p={{ md: SECTION_SPACING }}
+      py={SECTION_SPACING / 2}
+      h={'calc(100vh - 55px)'}
+    >
+      <Stack gap={SECTION_SPACING / 2} h={'100%'} justify={'center'}>
+        <Group justify="center">
+          <CardSessionLive session={selectedSession} />
+        </Group>
       </Stack>
     </Container>
   );

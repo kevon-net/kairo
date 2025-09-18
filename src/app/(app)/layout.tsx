743541af2b62a5d46777295d2d2a512cd3903d0f
Kevon Kibochi <dev@kevon.net>
2025-09-18 14:48:39 +0300

    fix: initialization react error

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,16 +5,20 @@ import ProviderStore from '@/components/providers/store';
 import ProviderSync from '@/components/providers/sync';
 import AppshellMain from '@/components/common/appshells/main';
 import ProviderView from '@/components/providers/view';
+import { createClient } from '@/libraries/supabase/server';
 
 export default async function LayoutApp({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = await createClient();
+  const { data: session } = await supabase.auth.getUser();
+
   return (
     <LayoutBody>
       <main>
-        <ProviderStore>
+        <ProviderStore session={session.user}>
           <ProviderSync>
             <AppshellMain>
               <ProviderView>{children}</ProviderView>
